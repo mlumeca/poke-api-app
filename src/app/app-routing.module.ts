914e10e34a40993a84626c2aa47320cbb535bff7
Luisa <mlumeca@gmail.com>
2024-10-23 20:42:04 +0200

    Move List complete

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,10 +3,12 @@ import { RouterModule, Routes } from '@angular/router';
 import { BerryListComponent } from './components/berry-list/berry-list.component';
 import { PokemonListComponent } from './components/pokemon-list/pokemon-list.component';
 import { PageNotFoundComponent } from './shared/page-not-found/page-not-found.component';
+import { MoveListComponent } from './components/move-list/move-list.component';
 
 const routes: Routes = [
   {path: 'berry', component: BerryListComponent},
   {path: 'pokemon', component: PokemonListComponent},
+  {path: 'move', component: MoveListComponent},
   {path: '', redirectTo: '/pokemon', pathMatch: 'full'},
   { path: '**', component: PageNotFoundComponent}
 ];
