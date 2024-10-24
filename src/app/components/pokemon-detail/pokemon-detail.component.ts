6e1a12bdbb1b11956c5590dfad44692ae1718f39
luisa <meron.camar24@triana.salesianos.edu>
2024-10-24 12:39:23 +0200

    Pokémon detail clase

--- src/app/components/pokemon-detail/pokemon-detail.component.ts
+++ src/app/components/pokemon-detail/pokemon-detail.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { PokemonDetailResponse } from '../../models/pokemon-detail.interface';
+import { PokemonService } from '../../services/pokemon.service';
 
 @Component({
   selector: 'app-pokemon-detail',
   templateUrl: './pokemon-detail.component.html',
   styleUrl: './pokemon-detail.component.css'
 })
-export class PokemonDetailComponent {
+export class PokemonDetailComponent implements OnInit {
+  pokemonId: string | null = '';
+  pokemon: PokemonDetailResponse | undefined;
 
+  constructor(
+    private route: ActivatedRoute,
+    private pokemonService: PokemonService
+  ) { }
+  
+  ngOnInit(): void {
+    debugger;
+    this.pokemonId = this.route.snapshot.paramMap.get('id');
+    debugger;
+    
+    this.pokemonService.getOnePokemon(parseInt(this.pokemonId!)).subscribe((response) => {
+      this.pokemon = response;
+    });
+  }
 }
