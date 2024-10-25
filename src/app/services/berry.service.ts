80ed435d9006a4096927cbe4fddcf9ca4d6d810f
candi <alcantarilla.cacan24@gmail.com>
2024-10-25 13:25:15 +0200

    terminado los detalles de bayas

--- src/app/services/berry.service.ts
+++ src/app/services/berry.service.ts
@@ -2,6 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { BerryListResponse } from '../models/berry-list.interface';
+import { BerryDetailResponse } from '../models/berry-detail-interface';
 
 @Injectable({
   providedIn: 'root'
@@ -13,4 +14,10 @@ export class BerryService {
   getBeeryList(): Observable<BerryListResponse> {
     return this.http.get<BerryListResponse>('https://pokeapi.co/api/v2/berry/');
   }
-}
+
+  getOneBerry(id: number): Observable<BerryDetailResponse> {
+    return this.http.get<BerryDetailResponse>(
+      `https://pokeapi.co/api/v2/berry/${id}/`
+    );
+  }
+} 
